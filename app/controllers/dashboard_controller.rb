fce9e4124e0a993390b2f22e481f5b7c1fa1d21b
tallowa <alubsey@gmail.com>
2025-09-19 19:41:26 -0400

    views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
+before_action :authenticate_user!
+
   def index
+    @organizations = current_user.organizations
+    @recent_time_entries = current_user.time_entries
+                                      .includes(:project)
+                                      .order(created_at: :desc)
+                                      .limit(10)
+    @active_time_entry = current_user.time_entries.in_progress.first
   end
 end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,19 +1,46 @@
 class OrganizationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_organization, only: [:show, :edit, :update]
+
   def index
+    @organizations = current_user.organizations
   end
 
   def show
+    @projects = @organization.projects
+    @recent_time_entries = @organization.time_entries
+                                       .includes(:user, :project)
+                                       .order(created_at: :desc)
+                                       .limit(20)
   end
 
   def new
+    @organization = Organization.new
   end
 
   def create
+    @organization = Organization.new(organization_params)
+    
+    if @organization.save
+      # Make the creator an owner
+      OrganizationMembership.create!(
+        user: current_user,
+        organization: @organization,
+        role: 'owner'
+      )
+      redirect_to @organization, notice: 'Organization created successfully!'
+    else
+      render :new
+    end
   end
 
-  def edit
+  private
+
+  def set_organization
+    @organization = current_user.organizations.find(params[:id])
   end
 
-  def update
+  def organization_params
+    params.require(:organization).permit(:name)
   end
 end
